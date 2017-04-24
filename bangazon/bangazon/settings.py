a80cce2f4e0ec594300fb641173352646b95dab1
William A Sims III <willsims14@gmail.com>
2017-04-24 10:26:14 -0500

    Changed name back

--- bangazon/bangazon/settings.py
+++ bangazon/bangazon/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'bangazon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'mydb.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
