beba2f9a5ac80fec39e23645cce49e2ac9a16f95
William A Sims III <willsims14@gmail.com>
2017-04-24 13:27:11 -0500

    Switched bundles views for urls.py

--- bangazon/bangazon/urls.py
+++ bangazon/bangazon/urls.py
@@ -2,16 +2,13 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
 
-from bangazon.api.views import views
+from bangazon.api.views import *
 
 router = routers.DefaultRouter()
 
-router.register(r'products', views.ProductViewSet)
-router.register(r'producttype', views.ProductTypeViewSet)
-router.register(r'customers', views.CustomerViewSet)
-
-
-
+router.register(r'products', ProductViewSet)
+router.register(r'producttype', ProductTypeViewSet)
+router.register(r'customers', CustomerViewSet)
 
 
 urlpatterns = [
