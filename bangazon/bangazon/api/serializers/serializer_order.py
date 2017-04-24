dd6f92638e8cbd90ec8e106ee864d78d0b9a63df
Jeremy Bakker <jeremybakker@Jeremys-MacBook-Pro.local>
2017-04-24 13:24:05 -0500

    removed lingering references to users and groups in view_order and serializer_order

--- bangazon/bangazon/api/serializers/serializer_order.py
+++ bangazon/bangazon/api/serializers/serializer_order.py
@@ -2,7 +2,6 @@
 bangazon api serializer configuration for order
 """
 
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from bangazon.api.models.model_order import *
 
--- bangazon/bangazon/api/views/view_order.py
+++ bangazon/bangazon/api/views/view_order.py
@@ -3,7 +3,6 @@ bangazon api view configuration for order
 """
 
 from django.shortcuts import render
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from bangazon.api.serializers.serializer_order import *
 from bangazon.api.models.model_order import *
