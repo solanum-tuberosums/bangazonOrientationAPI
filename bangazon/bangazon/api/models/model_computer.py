029c2496993546356c47cf21e386def515e81d8e
William A Sims III <willsims14@gmail.com>
2017-04-25 10:04:13 -0500

    Changed title to serial_number

--- bangazon/bangazon/api/models/model_computer.py
+++ bangazon/bangazon/api/models/model_computer.py
@@ -11,9 +11,10 @@ class Computer(models.Model):
 
     Author: Zak Spence
     """
-    title = models.CharField(max_length=50)
+    serial_number = models.CharField(max_length=50)
     date_purchased = models.DateField()
     date_decommissioned = models.DateField(blank=True, null=True)
 
+
     def __str__(self):
-        return self.name
+        return self.serial_number
