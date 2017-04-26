910eb49496d89a22d621f3fdc38c3da06dbd6a1b
Zak Spence <zakspence@gmail.com>
2017-04-26 15:20:33 -0500

    docstring of builddb explicitly states its purpose and the commands it calls

--- bangazon/bangazon/api/management/commands/builddb.py
+++ bangazon/bangazon/api/management/commands/builddb.py
@@ -9,7 +9,12 @@ from django.core.management.commands import makemigrations
 
 class Command(BaseCommand):
     """
-    Defines what the 'builddb' command should do
+    Defines the command 'builddb', which is a shortcut for running
+    all the necessary shell commands to generate our database's tables and
+    load our fixtures to our database. These commands are, in order:
+    1. python manage.py makemigrations api
+    2. python manage.py migrate
+    3. python manage.py loaddata <fixtures>
 
     Author: Jeremy Bakker
     """
@@ -17,6 +22,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         management.call_command('makemigrations', 'api')
         management.call_command('migrate')
-        management.call_command('loaddata', 'department', 'computer', 'training_program', 
+        management.call_command('loaddata', 'department', 'computer', 'training_program',
             'customer', 'employee', 'payment_type', 'product_type', 'product',
             'order', 'order_product', 'employee_training', 'employee_computer')
