acac0d7f6b58479bfb141b67bb73b736e85e7100
William A Sims III <willsims14@gmail.com>
2017-04-26 09:59:43 -0500

    Updated conditionals

--- bangazon/bangazon/api/management/commands/rmdb.py
+++ bangazon/bangazon/api/management/commands/rmdb.py
@@ -1,7 +1,18 @@
 import os
 from django.core.management.base import BaseCommand, CommandError
 
+
+
 class Command(BaseCommand):
+    """
+    Command: 'rmdb'
+    Purpose: to delete db.sqlite3 and the api/migrations/ directory. Our team used to
+              manually delete this file and this directory so we wrote a custom command
+              to simplify the process.
+    Author: Will Sims
+    """
+
+
     help = 'Deletes the local copies of the db.sqlite3 file and the bangazon/api/migrations/ directory.'
 
     def handle(self, *args, **options):
@@ -13,7 +24,7 @@ class Command(BaseCommand):
         except:
             migrations_deleted = "FALSE"
 
-        if os.path.isfile('db.sqlite3'):
+        try:
             os.system("rm db.sqlite3")
             db_deleted = "TRUE"
             self.stdout.write("""
@@ -27,7 +38,9 @@ class Command(BaseCommand):
              ----------------------------------------------
              ----------------------------------------------
              """.format(migrations_deleted, db_deleted))
-        else:
+        except:
             db_deleted = "FALSE"
             self.stdout.write("\n \n      BANGAZON: Database does not exist, so it could not be deleted.\n ")
+
+
 
