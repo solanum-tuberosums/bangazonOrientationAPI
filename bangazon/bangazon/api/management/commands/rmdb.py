bbff5892006b686d63b2507a1803c8a7b1670976
William A Sims III <willsims14@gmail.com>
2017-04-25 19:12:47 -0500

    Added command to remove db.sqlite3 and /api/migrations/ directory

---
+++ bangazon/bangazon/api/management/commands/rmdb.py
@@ -0,0 +1,40 @@
+import os
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    help = 'Deletes the local copies of the db.sqlite3 file and the bangazon/api/migrations/ directory.'
+
+    def handle(self, *args, **options):
+
+        if os.path.exists("bangazon/api/migrations/") and os.path.isdir("bangazon/api/migrations/"):
+            os.system("rm -rf bangazon/api/migrations/")
+            migrations_deleted = "TRUE "
+        else:
+            migrations_deleted = "FALSE"
+
+        if os.path.isfile('db.sqlite3'):
+            os.system("rm db.sqlite3")
+            db_deleted = "TRUE"
+            self.stdout.write("""
+             ----------------------------------------------
+             -----------------  SUCCESS   -----------------
+             --- ------------------------------------------
+             ---                                        ---
+             ---   - Migrations Deleted:        {}   ---
+             ---   - Database Deleted:          {}    ---
+             ---                                        ---
+             ----------------------------------------------
+             ----------------------------------------------
+             """.format(migrations_deleted, db_deleted))
+        else:
+            db_deleted = "FALSE"
+            self.stdout.write("""
+             ------------------------------------------------------------------
+             -------------------   DATABASE DOESN'T EXIST   -------------------
+             --- --------------------------------------------------------------
+             ---                                                            ---
+             ---    The database does not exist so it cannot be deleted.    ---
+             ---                                                            ---
+             ------------------------------------------------------------------
+             ------------------------------------------------------------------
+             """.format(migrations_deleted, db_deleted))
