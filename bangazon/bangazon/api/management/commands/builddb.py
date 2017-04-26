a1b063b1da979e311345d66e0f0bc5a7995756ec
Jeremy Bakker <jeremybakker@Jeremys-MacBook-Pro.local>
2017-04-26 14:30:54 -0500

    command line now runs with management.call_command rather than os

--- bangazon/bangazon/api/management/commands/builddb.py
+++ bangazon/bangazon/api/management/commands/builddb.py
@@ -2,32 +2,21 @@
 bangazon api custom command to build database
 """
 
-from django.core.management.base import BaseCommand, CommandError
-import os
+from django.core import management
+from django.core.management.base import BaseCommand
+from django.core.management.commands import makemigrations
 
 
 class Command(BaseCommand):
     """
     Defines what the 'builddb' command should do
-    and what the '--help' option will display to the user.
 
-    Author: Blaise Roberts
+    Author: Jeremy Bakker
     """
 
-    help = 'Builds and populates your database tables.'
-
     def handle(self, *args, **options):
-        os.system("python manage.py makemigrations api")
-        os.system("python manage.py migrate")
-        os.system("python manage.py loaddata department")
-        os.system("python manage.py loaddata computer")
-        os.system("python manage.py loaddata training_program")
-        os.system("python manage.py loaddata customer")
-        os.system("python manage.py loaddata product_type")
-        os.system("python manage.py loaddata employee")
-        os.system("python manage.py loaddata payment_type")
-        os.system("python manage.py loaddata product")
-        os.system("python manage.py loaddata order")
-        os.system("python manage.py loaddata order_product")
-        os.system("python manage.py loaddata employee_training")
-        os.system("python manage.py loaddata employee_computer")
+        management.call_command('makemigrations', 'api')
+        management.call_command('migrate')
+        management.call_command('loaddata', 'department', 'computer', 'training_program', 
+            'customer', 'employee', 'payment_type', 'product_type', 'product',
+            'order', 'order_product', 'employee_training', 'employee_computer')
