93a0f66c28be01739bf373f69c7d91880a89505f
Adam Myers <ANMyers53@yahoo.com>
2017-04-27 13:13:02 -0500

    added a return __str__ to order model for viewing

--- bangazon/bangazon/api/models/model_order.py
+++ bangazon/bangazon/api/models/model_order.py
@@ -7,17 +7,20 @@ from bangazon.api.models import *
 
 
 class Order(models.Model):
-	"""
-	This class models an order in the API's database.
+    """
+    This class models an order in the API's database.
 
-	----Fields----
-	payment_type_id(decimal): Refers to PaymentType(PaymentTypeID) without a foreign key
-	order_date(date): Refers to the date an order is made.
-	customer_id(foreign key): Links to Customer(CustomerID) with a foreign key
+    ----Fields----
+    payment_type_id(decimal): Refers to PaymentType(PaymentTypeID) without a foreign key
+    order_date(date): Refers to the date an order is made.
+    customer_id(foreign key): Links to Customer(CustomerID) with a foreign key
 
-	Author: Jeremy Bakker
-	"""
+    Author: Jeremy Bakker
+    """
 
-	payment_type_id = models.DecimalField(max_digits=20, decimal_places=0, blank=True, null=True)
-	order_date = models.DateField()
-	customer_id = models.ForeignKey(Customer)
+    payment_type_id = models.DecimalField(max_digits=20, decimal_places=0, blank=True, null=True)
+    order_date = models.DateField()
+    customer_id = models.ForeignKey(Customer)
+
+    def __str__(self):
+        return "Order #{} created on {} by {}".format(self.pk, self.order_date, self.customer_id)
